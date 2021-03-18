93c7fdca85d2f5a74ead12fac65267d6a530b127
Mano <Mano@Dell>
2021-03-18 08:04:36 +0530

    My Second commit

---
+++ SampleProgramForTestGitHub/src/com/SampleProgramForTestGitHub/SampleProgram.java
@@ -0,0 +1,14 @@
+package com.SampleProgramForTestGitHub;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		String s1="Mano";
+		String s2="Karan";
+		String s;
+		s=s1+s2;
+		System.out.println(s);
+		
+	}
+
+}
